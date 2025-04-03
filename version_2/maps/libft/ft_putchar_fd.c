1e35e9eb9bfdff613f3a76f2713c67d3582956b2
Samy <161335330+BejaouiSamy@users.noreply.github.com>
2025-04-03 12:14:53 +0200

    Delete version_2 directory

--- version_2/maps/libft/ft_putchar_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-/*Cette fonction permet d'ecrire un caractere
-a un emplacement specifie, que ce soit sortie
-standard, un fichier ou autre flux de sortie.*/
